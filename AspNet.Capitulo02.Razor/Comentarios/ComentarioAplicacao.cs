f9036f6f36c09dfc7eb1770f8d0303501ec0e473
gustavo-pupo <gusfepupo@gmail.com>
2021-09-23 22:07:09 -0300

    Adicionar arquivos de projeto.

---
+++ AspNet.Capitulo02.Razor/Comentarios/ComentarioAplicacao.cs
@@ -0,0 +1,44 @@
+﻿using AspNet.Capitulo02.Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace AspNet.Capitulo02.Razor.Comentarios
+{
+    public class ComentarioAplicacao
+    {
+        private readonly string caminho = AppDomain.CurrentDomain.BaseDirectory + ConfigurationManager.AppSettings["caminhoArquivoComentario"];
+
+        public void Inserir(string nome, string comentario)
+        {
+            File.AppendAllText(caminho, $"{DateTime.Now}|{nome}|{comentario}" + Environment.NewLine);
+        }
+
+        public List<ComentarioViewModel> Selecionar()
+        {
+            var comentarios = new List<ComentarioViewModel>();
+
+            foreach (var linha in File.ReadAllLines(caminho))
+            {
+                if (linha == string.Empty)
+                {
+                    continue;
+                }
+                var propriedades = linha.Split('|');
+
+                var comentario = new ComentarioViewModel();
+                comentario.Data = Convert.ToDateTime(propriedades[0]);
+                comentario.Comentarista = propriedades[1];
+                comentario.Comentario = propriedades[2];
+
+                comentarios.Add(comentario);
+            }
+
+
+            return comentarios;
+        }
+    }
+}
---
+++ AspNet.Capitulo02.Razor/Models/ComentarioViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNet.Capitulo02.Razor.Models
+{
+    public class ComentarioViewModel
+    {
+        public DateTime Data { get; set; }
+        public string Comentarista { get; set; }
+        public string Comentario { get; set; }
+    }
+}
