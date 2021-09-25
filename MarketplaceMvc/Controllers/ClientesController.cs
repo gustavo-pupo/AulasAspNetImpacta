7685737dff2d4b80ffd04e0cc75b02e76d3f3278
gustavo-pupo <gusfepupo@gmail.com>
2021-09-24 22:54:52 -0300

    Colocando e configurando insert no MVC

---
+++ MarketplaceMvc/Controllers/ClientesController.cs
@@ -0,0 +1,116 @@
+﻿
+using MarketplaceMvc.Models;
+using MarketplaceRepositoriosSqlServerDbFirst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarketplaceMvc.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly ClienteRepositorio clienteRepositorio = new ClienteRepositorio();
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        public ActionResult Create(ClienteViewModel viewModel)
+        {
+            try
+            {
+                //var documento = collection["documento"]; 
+                if (!ModelState.IsValid)
+                {
+                    return View(viewModel);
+                }
+
+                clienteRepositorio.Inserir(Mapear(viewModel));
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Error");
+            }
+        }
+
+        private Cliente Mapear(ClienteViewModel viewModel)
+        {
+            var cliente = new Cliente();
+
+            //AutoMapper
+            cliente.Documento = viewModel.Documento;
+            cliente.Email = viewModel.Email;
+            cliente.Id = viewModel.Id;
+            cliente.Nome = viewModel.Nome;
+            cliente.Telefone = viewModel.Telefone;
+
+
+
+            return cliente;
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MarketplaceMvc/Models/ClienteViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MarketplaceMvc.Models
+{
+    public class ClienteViewModel
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(14)]
+        public string Documento { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
