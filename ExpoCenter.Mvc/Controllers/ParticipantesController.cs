e5b5029a1002f782d4488258add12d6b491e23a2
gustavo-pupo <gusfepupo@gmail.com>
2021-10-06 19:47:58 -0300

    novo mvc com asp net core

---
+++ ExpoCenter.Mvc/Controllers/ParticipantesController.cs
@@ -0,0 +1,94 @@
+﻿using ExpoCenter.Repositorios.SqlServer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpoCenter.Mvc.Controllers
+{
+    public class ParticipantesController : Controller
+    {
+        private readonly ExpoCenterDbContext dbContext;//= new ExpoCenterDbContext();
+        public ParticipantesController()
+        {
+            
+        }
+
+        // GET: ParticipantesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ParticipantesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ParticipantesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ParticipantesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ParticipantesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ParticipantesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ParticipantesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ParticipantesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ExpoCenter.Mvc/Models/ParticipanteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpoCenter.Mvc.Models
+{
+    public class ParticipanteViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+
+        [Display(Name = "Nascimento")]
+        public DateTime DataNascimento { get; set; } 
+        
+
+    }
+}
